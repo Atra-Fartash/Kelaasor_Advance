ad31693b9e9155c5ee7de42243aa92886f56c0fa
Atra-Fartash <atrafartash@gmail.com>
2025-10-25 13:16:45 +0330

    add admin

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from course.models import Category, Teacher, Course,  Comment, Profile, Basket, BasketItem, Wallet, Transaction, Ticket, TicketMessage
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course_type', 'price']
+    search_fields = ['title']
+    list_filter = ['category']
+
+
+admin.site.register(Category)
+admin.site.register(Teacher)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Basket)
+admin.site.register(BasketItem)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
+admin.site.register(Ticket)
+admin.site.register(TicketMessage)
